ab9e9c41401f3889423dc2b009325e63828949dd
kaloysui <ysc22tdtnk@privaterelay.appleid.com>
2026-03-18 20:24:03 +0800

    Add ecosystem configuration for production app

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "bcine",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
+      instances: 1,              // pwede 'max' kung gusto nimo cluster mode
+      autorestart: true,         // auto-restart kung mo-crash
+      watch: false,              // disable sa production
+      max_memory_restart: "800M", // restart if memory exceeds 200MB
+      env: {
+        NODE_ENV: "production"
+      }
+    }
+  ]
+};
