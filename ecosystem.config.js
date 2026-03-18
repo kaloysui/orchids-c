c823410d6ec385f980208e7e73c5b34e94409fd1
kaloysui <ysc22tdtnk@privaterelay.appleid.com>
2026-03-18 20:25:29 +0800

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
       name: "bcine",
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
-      instances: 1,              // pwede 'max' kung gusto nimo cluster mode
+      instances: max,              // pwede 'max' kung gusto nimo cluster mode
       autorestart: true,         // auto-restart kung mo-crash
       watch: false,              // disable sa production
       max_memory_restart: "800M", // restart if memory exceeds 200MB
