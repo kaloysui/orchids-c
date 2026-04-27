2f57e3969112a22a39b40101298846d8e730502a
kaloysui <ysc22tdtnk@privaterelay.appleid.com>
2026-04-27 20:50:31 +0800

    Include Google Sans font in layout
    Added Google Sans font to the layout.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,7 +38,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`antialiased ${inter.variable} ${poppins.variable}`}>
+      <head>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Google+Sans:wght@400;500;700&display=swap" rel="stylesheet" />
+      </head>
+      <body className={`antialiased ${inter.variable} ${poppins.variable}`} style={{ fontFamily: "'Google Sans', sans-serif" }}>
 
         <ErrorReporter />
     
