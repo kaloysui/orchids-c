f58fe11c487e4282247433a063a68864c7a43338
kaloysui <ysc22tdtnk@privaterelay.appleid.com>
2026-04-29 18:30:51 +0800

    Change exec_mode from cluster to fork

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,7 @@ module.exports = {
       script: "node_modules/next/dist/bin/next",
       args: "start -p 3000",
       instances: "max",           // gamiton tanan CPU cores
-      exec_mode: "cluster",       // cluster mode for load balancing
+      exec_mode: "fork",       // cluster mode for load balancing
       autorestart: true,          // auto-restart kung mo-crash
       watch: false,               // disable watch sa production
       max_memory_restart: "2G", // auto-restart kung mo-abot sa 300MB
